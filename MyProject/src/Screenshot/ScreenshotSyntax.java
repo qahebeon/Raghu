fa4c45cb858083ef2af749ffa9db9fc69d4c3d4c
dell <dell@dell-PC.hebeon.in>
2018-08-31 17:05:25 +0530

    Screenshot program updated

--- MyProject/src/Screenshot/ScreenshotSyntax.java
+++ MyProject/src/Screenshot/ScreenshotSyntax.java
@@ -1,5 +1,12 @@
 package Screenshot;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -7,12 +14,14 @@ import org.testng.annotations.Test;
 public class ScreenshotSyntax {
 	
 	@Test
-	public static void captureScreen(){
-		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"Drivers/geckodriver.exe");
+	public static void captureScreen() throws IOException{
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/Drivers/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("https://qatesting.hebeon.com");
-		//File f = (TakesScreenshot)driver).
+		driver.get("https://google.com");
+		File f = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(f, new File("C:\\Users\\dell\\Desktop\\Screenshots.png"));	
+		driver.close();
 	}
 
 }
